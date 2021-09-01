3a66076d6507b77dabba6a26338e9a6638b17760
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-01 16:19:58 +0530

    ADDED: Welcome Message

---
+++ src/com/bridgelabz/mybinarytree/MyBinaryTreeMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.mybinarytree;
+
+public class MyBinaryTreeMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("------ Welcome to Data Structure : Binary Search Tree ------");
+
+	}
+
+}
